549d6b1f09a74848b3b43a6e4bc2644819e92e8f
Aslak Knutsen <aslak@4fs.no>
2019-11-19 04:18:31 +0100

    demo: add custom domain

--- workshop/content/scripts/deploy.sh
+++ workshop/content/scripts/deploy.sh
@@ -7,13 +7,15 @@ MYHOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd -P)"
 DEPLOYMENT_DIR="${MYHOME}/src/deployments"
 
 # name of project in which we are working
-PROJECT=${PROJECT:-istio-tutorial}
-ISTIO_NS=${ISTIO_NS:-istio-system}
+PROJECT=${PROJECT:-workspace-demo}
+ISTIO_NS=${ISTIO_NS:-service-mesh-1}
 
 oc new-project ${PROJECT} || true
 oc adm policy add-scc-to-user privileged -z default -n ${PROJECT}
 oc adm policy add-scc-to-user anyuid -z default -n ${PROJECT}
 oc get ServiceMeshMemberRoll default -n ${ISTIO_NS} -o json | jq --arg PROJECT "${PROJECT}" '.spec.members[.spec.members | length] |= $PROJECT' | oc apply -f - -n ${ISTIO_NS}
+
+IKE_DOCKER_REGISTRY=docker.io IKE_DOCKER_REPOSITORY=aslakknutsen NAMESPACE=${PROJECT} IKE_IMAGE_TAG=latest ike install-operator -l -n ${PROJECT}
 
 # deploy gateway
 oc create -n ${PROJECT} -f ${DEPLOYMENT_DIR}/gateway.yaml
@@ -26,3 +28,6 @@ oc create -n ${PROJECT} -f ${DEPLOYMENT_DIR}/preference.yaml
 
 # deploy recommendation
 oc create -n ${PROJECT} -f ${DEPLOYMENT_DIR}/recommendation.yaml
+
+# deploy fake hosts
+oc apply -n ${PROJECT} -f ${DEPLOYMENT_DIR}/fake-vs.yaml
