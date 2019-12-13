587e8b6cdc44056147e32eb9e5b393b02c63b77c
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-12-13 09:52:09 +0100

    fix: points to ike-demo.io in curl scripts

--- local.sh
+++ local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo -e "\033[32;7m##### LOCAL               \e[0m";
 
-curl -s feature-y.ike-bjug.io
+curl -s feature-y.ike-demo.io
--- pr.sh
+++ pr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo -e "\033[34;7m##### PULL REQUEST               \e[0m";
 
-curl -s feature-x.ike-bjug.io
+curl -s feature-x.ike-demo.io
