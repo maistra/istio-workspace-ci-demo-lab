48f624acc342fbe956c460295d89cb50004a0339
Bartosz Majsak <bartosz.majsak@gmail.com>
2020-01-31 14:33:49 +0100

    feat: improved response

--- workshop/content/src/recommendation/src/main/java/com/redhat/developer/demos/recommendation/rest/RecommendationResource.java
+++ workshop/content/src/recommendation/src/main/java/com/redhat/developer/demos/recommendation/rest/RecommendationResource.java
@@ -16,7 +16,7 @@ import org.slf4j.LoggerFactory;
 @Path("/")
 public class RecommendationResource {
 
-    private static final String RESPONSE_STRING_FORMAT = "recommendation %s from '%s': %d\n";
+    private static final String RESPONSE_STRING_FORMAT = "GH PR recommendation %s from '%s': %d\n";
 
     private static final String RESPONSE_STRING_NOW_FORMAT = "recommendation v3 %s from '%s': %d\n";
 
