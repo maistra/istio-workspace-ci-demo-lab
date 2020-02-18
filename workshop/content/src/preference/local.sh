21ff1034bd083afb8834016f013f3095bfa90969
Aslak Knutsen <aslak@4fs.no>
2020-02-19 00:19:20 +0100

    demo: force ipv4 stack to enable inject-tcp

--- workshop/content/src/preference/local.sh
+++ workshop/content/src/preference/local.sh
@@ -1,1 +1,2 @@
-ike develop -d preference-v1 -p 8080 -n workspace-demo --route header:ike-session-id=feature-p -r "mvn compile quarkus:dev"
+export JAVA_TOOL_OPTIONS='-Djava.net.preferIPv4Stack=true'
+ike develop -d preference-v1 -p 8080 -n workspace-demo -m inject-tcp --route header:ike-session-id=feature-z -r "mvn compile quarkus:dev"
--- workshop/content/src/recommendation/local.sh
+++ workshop/content/src/recommendation/local.sh
@@ -1,1 +1,2 @@
+export JAVA_TOOL_OPTIONS='-Djava.net.preferIPv4Stack=true'
 ike develop -d recommendation-v1 -p 8080 -n workspace-demo --route header:ike-session-id=feature-y -r "mvn compile quarkus:dev"
